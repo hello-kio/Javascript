ba5dabd23e7b8342b1b08e10eff7f3f960f7d2c4
k-io <bhishek29012003@gmail.com>
2024-04-30 00:44:56 +0530

    updates basic file

--- Basics/test_datatypes_convertors.js
+++ Basics/test_datatypes_convertors.js
@@ -1,3 +1,25 @@
 // Data types and conversions :-
 
-let age = 
+//strng , number , boolean ->for conversion String , Number , Boolean
+
+let age = "23" ;
+let conv = Number(age);
+
+//above casting feature converts the variable in age to number if it can 
+// and if it can't then console.log return NaN(not a number);
+
+console.log(typeof(conv));
+console.log(conv);
+
+//true will be converted to 1 and false will be to 0 ;
+// null will be converted to 0 ;
+//undefined will be converted to NaN (but the type of NaN will be a number only)
+
+//1=> true if we convert to boolean 
+//""=> false; but "xyz"=> true ;
+//33--> string conversion makes it 33 only lol 
+// but the type gets changes to string 
+
+// ------------------->Operations<---------------------
+
+
