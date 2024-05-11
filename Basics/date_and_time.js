a410ce52d123e2f89074d1ed686306ba7b9aae2d
k-io <bhishek29012003@gmail.com>
2024-05-11 16:54:23 +0530

    dates_remaining

---
+++ Basics/date_and_time.js
@@ -0,0 +1,13 @@
+//Dates;
+let myDate = new Date();//date is an abject in Js
+
+console.log(myDate);
+
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+
+let createdDate = new Date(2024 , 4 , 10)
+//0 indexing is followed in months
+console.log(createdDate.toDateString());
+
+
