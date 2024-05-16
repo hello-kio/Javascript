27a95f17ea812dbef517c75249d8ea6d4a861536
k-io <bhishek29012003@gmail.com>
2024-05-16 14:09:12 +0530

    basics

--- functions/file_01.js
+++ functions/file_01.js
@@ -34,3 +34,28 @@ function loginMessage(userName = "General user"){
     return `${userName} just loggedin in`;
 }//this sets the default username as : general user ; so even if no value is passed in the function while calling it then also it will not be undefined  ; bcz when it will be passed the value will be overwritten ;
 
+//spread operator is rest operator also it depends on where it is used that it will be called spread or rest operator
+
+function calculatePrice(...num){
+    return num
+}
+console.log(calculatePrice(132,12,765,1,23,54,2));//we passed so mant variables but the function only accepts 1 variable so now we use the rest operator that converts the output to an array of all the input values 
+
+//how to pass objects through function ;
+const obj1 = {
+    name : "player1",
+    age: 25,
+    status : 'bronze'
+}
+function objpassing(my_object){
+    console.log(`${my_object.name} has been promoted to ${my_object.status}`);
+}
+
+objpassing(obj1);
+//objpassing({           //this can also be done like this
+//     name : "player1",
+//     age: 25,
+//     status : 'bronze'
+// })
+
+//console.log is printing the o/p and return is returning from the function ;
